2a31dd334f985371c5c89804a9f3dd7a06f5714f
DavidCarrilho <luan.david.oliver@gmail.com>
2022-07-10 15:32:03 -0300

    chore: add libs

--- bin/backend.dart
+++ bin/backend.dart
@@ -2,6 +2,6 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void main() async {
-  final server = await shelf_io.serve((request) => Response(200, body: 'ok'), "localhost", 8080);
+  final server = await shelf_io.serve((request) => Response(200, body: 'ok!'), "localhost", 8080);
   print('Nosso servidor foi iniciado http:localhost:8080');
 }
