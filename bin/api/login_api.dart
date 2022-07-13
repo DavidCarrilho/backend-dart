11bdf00f0d907ddf01c882e2501bad726aa66166
DavidCarrilho <luan.david.oliver@gmail.com>
2022-07-13 01:49:39 -0300

    chore: create login api

---
+++ bin/api/login_api.dart
@@ -0,0 +1,13 @@
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class LoginApi {
+  Handler get handler {
+    Router router = Router();
+    router.post('/login', (Request req) {
+      return Response.ok("Api de Login");
+    });
+    return router;
+  }
+}
--- bin/backend.dart
+++ bin/backend.dart
@@ -1,6 +1,7 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+import 'api/login_api.dart';
 import 'infra/custom_server.dart';
 import 'server_handler.dart';
 
@@ -9,5 +10,5 @@ void main() async {
   // final server = await shelf_io.serve(_server.handler, "localhost", 8080);
   // print('Nosso servidor foi iniciado http:localhost:8080');
 
-  // CustomServer().initialize((request) => null);
+  await CustomServer().initialize((LoginApi().handler));
 }
