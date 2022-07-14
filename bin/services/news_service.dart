4431accf06bf52f432088d067e49b1d78402bc50
DavidCarrilho <luan.david.oliver@gmail.com>
2022-07-14 18:09:03 -0300

    chore: create genric and news services

---
+++ bin/services/news_service.dart
@@ -0,0 +1,28 @@
+import '../models/news_model.dart';
+import 'generic_service.dart';
+
+class NewsService implements GenericService<NewsModel>{
+  @override
+  bool delete(NewsModel value) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  List<NewsModel> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+
+  @override
+  NewsModel findOne(int id) {
+    // TODO: implement findOne
+    throw UnimplementedError();
+  }
+
+  @override
+  bool save(NewsModel value) {
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+}
