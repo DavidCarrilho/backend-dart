1ce0daaee1af432246158c761b8b3bac2d16ce53
DavidCarrilho <luan.david.oliver@gmail.com>
2022-07-14 16:08:27 -0300

    feat: create endpoints post, put and delete in the api

--- bin/api/blog_api.dart
+++ bin/api/blog_api.dart
@@ -4,7 +4,22 @@ import 'package:shelf_router/shelf_router.dart';
 class BlogApi {
   Handler get handler {
     Router router = Router();
+    // Listagem
     router.get('/blog/noticias', (Request req) {
+      return Response.ok('Choveu hoje');
+    });
+    // Nova noticia
+    router.post('/blog/noticias', (Request req) {
+      return Response.ok('Choveu hoje');
+    });
+    // Uma atualizacao / ex: /blog/noticias?id=1
+    router.put('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
+    // Uma exclusao / ex: /blog/noticias?id=1
+    router.delete('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
       return Response.ok('Choveu hoje');
     });
     return router;
