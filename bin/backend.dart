0e90040c920ce25d43be7acc3c235172422d7d28
DavidCarrilho <luan.david.oliver@gmail.com>
2022-07-16 18:03:51 -0300

    refactor: code refactor in tht backend

--- bin/backend.dart
+++ bin/backend.dart
@@ -5,6 +5,7 @@ import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
 import 'infra/middleware_interception.dart';
+import 'infra/security/security_service.dart';
 import 'infra/security/security_service_impl.dart';
 import 'services/news_service.dart';
 import 'utils/custom_env.dart';
@@ -13,21 +14,23 @@ void main() async {
   // var _server = CustomServerHandler();
   // final server = await shelf_io.serve(_server.handler, "localhost", 8080);
   // print('Nosso servidor foi iniciado http:localhost:8080');
-  
-  // usar o kdebug 
+
+  // usar o kdebug
   CustomEnv.fromFile('.env-dev');
 
+  SecurityService _securityService = SecurityServiceImpl();
+
   var cascadeHandler = Cascade()
-    .add(LoginApi(SecurityServiceImpl()).handler)
-    .add(BlogApi(NewsService()).handler).handler;
+      .add(LoginApi(_securityService).handler)
+      .add(BlogApi(NewsService()).handler)
+      .handler;
 
   var handler = Pipeline()
-    .addMiddleware(logRequests())
-    .addMiddleware(MiddlewareIntercption().middleware)
-    .addMiddleware(SecurityServiceImpl().authorization)
-    .addMiddleware(SecurityServiceImpl().verifyJwt)
-    .addHandler(cascadeHandler);
-
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareIntercption().middleware)
+      .addMiddleware(_securityService.authorization)
+      .addMiddleware(_securityService.verifyJwt)
+      .addHandler(cascadeHandler);
 
   await CustomServer().initialize(
     handler: handler,
